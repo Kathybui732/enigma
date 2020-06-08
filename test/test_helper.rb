256615d1b2cc25e9bd01f1c8fdb73d763705a7f9
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 20:50:09 -0600

    Create test helper

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "date"
+require "./lib/key"
+require "./lib/date"
+require "./lib/alphabet"
+require "./lib/message"
+require "./lib/enigma"
