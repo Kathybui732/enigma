674d170d468261fb5626487fbf055e5e9b0f436c
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 22:46:07 -0600

    Fix spacing

--- test/key_test.rb
+++ test/key_test.rb
@@ -3,13 +3,13 @@ require_relative "./test_helper"
 class KeyTest < Minitest::Test
 
   def setup
-		@key = Key.new("02715")
-	end
+    @key = Key.new("02715")
+  end
 
   def test_it_exists_with_attributes
     assert_instance_of Key, @key
-		assert_equal "02715", @key.code
-	end
+    assert_equal "02715", @key.code
+  end
 
   def test_it_can_create_shift_keys
     assert_equal [2, 27, 71, 15], @key.shift_keys
