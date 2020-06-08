6bef5c23e607d49ab1882d0eb78d6e238403746b
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 21:55:48 -0600

    Create breakdown method

--- lib/message.rb
+++ lib/message.rb
@@ -5,4 +5,16 @@ class Message
     @content = content
     @encrypted_content = []
   end
+
+  def breakdown
+    snippets = @content.chars.each_slice(4).map(&:join)
+    array = snippets.map do |element|
+      element.chars
+    end
+    breakdown = array.map do |array|
+      array.map do |letter|
+        Alphabet.new(letter)
+      end
+    end
+  end
 end
