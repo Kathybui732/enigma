8304f61b135a81dbd83eebe6b6eef2bbf83d7948
Kathy B <61847658+Kathybui732@users.noreply.github.com>
2020-06-07 21:17:01 -0600

    Update date_test.rb
    Fix spacing

--- test/date_test.rb
+++ test/date_test.rb
@@ -2,9 +2,9 @@ require_relative "./test_helper"
 
 class DateCodeTest < Minitest::Test
 
-	def setup
-		@date_1 = DateCode.new("040895")
-	end
+  def setup
+    @date_1 = DateCode.new("040895")
+  end
 
   def test_it_exists_with_attributes
     assert_equal "040895", @date_1.code
