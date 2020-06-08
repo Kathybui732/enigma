52994b45010366b00bbcd38e4262a6b6b999d29b
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 21:21:26 -0600

    Create test for attributes

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -8,4 +8,19 @@ class AlphabetTest < MiniTest::Test
 		@punctuation = Alphabet.new("!")
 		@capitalize = Alphabet.new("K")
 	end
+
+  def test_it_exists_with_attributes
+    assert_instance_of Alphabet, @alphabet
+    assert_equal "k", @alphabet.character
+    assert_equal " ", @space.character
+    assert_equal "!", @punctuation.character
+    assert_equal "k", @capitalize.character
+    assert_equal true, @alphabet.character.downcase == @alphabet.character
+    assert_equal true, @capitalize.character.downcase == @capitalize.character
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @alphabet.valid_alphabet
+    assert_equal true, @space.valid_alphabet.include?(@space.character)
+    assert_equal false, @punctuation.valid_alphabet.include?(@punctuation.character)
+    assert_equal true, @capitalize.valid_alphabet.include?(@capitalize.character)
+  end
 end
