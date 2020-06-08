f9e8051d104c7bcf22128d577548577af707d08c
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 22:13:29 -0600

    Create test for key object

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -11,4 +11,10 @@ class EnigmaTest < MiniTest::Test
 	def test_it_exists
 		assert_instance_of Enigma, @enigma
 	end
+
+  def test_it_can_get_a_key_object
+    assert_instance_of Key, @enigma.key_object("02715")
+    assert_instance_of Key, @enigma.key_object(nil)
+    assert_instance_of Key, @enigma.key_object(Key.new("02715"))
+  end
 end
