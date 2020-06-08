77e2544c61461f107164d9ea110ba171523e73b2
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 21:00:09 -0600

    Create shift keys method

--- lib/key.rb
+++ lib/key.rb
@@ -5,4 +5,11 @@ class Key
     @code = code
   end
 
+  def shift_keys
+    shift_keys = []
+    @code.chars.each_cons(2) do |pair|
+      shift_keys << pair.join.to_i
+    end
+    shift_keys
+  end
 end
