7dc4889156a9224ac3a5e4994651f3a0ac50bf2d
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 22:14:02 -0600

    Create key object method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,4 +6,15 @@ class Enigma
     @key = nil
     @date = nil
   end
+
+  def key_object(key)
+    if key.nil?
+      random_key = rand(00000..99999).to_s.rjust(5, "0")
+      @key = Key.new(random_key)
+    elsif key.is_a?(Key)
+      @key = key
+    else
+      @key = Key.new(key)
+    end
+  end
 end
