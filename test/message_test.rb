d26988fbd6e2743e81068e0caf1892fe448dfd97
Kathy B <61847658+Kathybui732@users.noreply.github.com>
2020-06-07 22:06:39 -0600

    Update message_test.rb
    Fixed spacing

--- test/message_test.rb
+++ test/message_test.rb
@@ -2,25 +2,25 @@ require_relative "./test_helper"
 
 class MessageTest < Minitest::Test
 
-	def setup
-		@message = Message.new("Hello, world!")
-		@encrypted_message = Message.new("keder,sprrdx!")
-	end
+  def setup
+    @message = Message.new("Hello, world!")
+    @encrypted_message = Message.new("keder,sprrdx!")
+  end
 
-	def test_it_exists_with_attributes
+  def test_it_exists_with_attributes
     assert_instance_of Message, @message
-		assert_equal "Hello, world!", @message.content
-	end
+    assert_equal "Hello, world!", @message.content
+  end
 
   def test_it_can_breakdown_the_message
-		all_obj = @message.breakdown.all? do |array|
-			array.all? do |element|
-				element.is_a?(Alphabet)
-			end
-		end
-		assert_equal true, all_obj
-		assert_equal 4, @message.breakdown.count
-	end
+    all_obj = @message.breakdown.all? do |array|
+      array.all? do |element|
+        element.is_a?(Alphabet)
+      end
+    end
+    assert_equal true, all_obj
+    assert_equal 4, @message.breakdown.count
+  end
 
   def test_it_can_encrypt
     expected = "keder,sprrdx!"
