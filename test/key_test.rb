99433d879f12016998b26be545e44ab0ec1897b3
Kathy Bui <kathy.bui1992@gmail.com>
2020-06-07 20:57:10 -0600

    Create test for shift keys

--- test/key_test.rb
+++ test/key_test.rb
@@ -9,4 +9,8 @@ class KeyTest < Minitest::Test
   def test_it_exists_with_attributes
 		assert_equal "02715", @key.code
 	end
+
+  def test_it_can_create_shift_keys
+    assert_equal [2, 27, 71, 15], @key.shift_keys
+  end
 end
